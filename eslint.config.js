0796fb1d6a76c2652739fbc7dcbb124412f980ae
Bryan Gordon <bryanbge92@gmail.com>
2025-02-14 22:31:43 -0500

    Config Neostandard linter

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,10 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { neostandard } from 'neostandard/lib/main.js'
 
 export default tseslint.config(
+  ...neostandard(),
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
