c3e07d533185ea64bada7473758d3866eec80b74
Bryan Gordon <bryanbge92@gmail.com>
2025-02-18 18:34:39 -0500

    Create component

---
+++ src/views/DataFinance.tsx
@@ -0,0 +1,21 @@
+export function DataFinance () {
+  return (
+    <>
+      <article>
+        <label>Ingrese el monto recibido:</label>
+        <input
+          type='number'
+          placeholder='500'
+        />
+        <textarea>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, autem.
+        </textarea>
+      </article>
+
+      <article>
+        <span>Total recibido en el mes:</span>
+        <span>$1200</span>
+      </article>
+    </>
+  )
+}
