a0c8948c2399a4bdf7c085032b8524b3d6d4d59e
Bryan Gordon <bryanbge92@gmail.com>
2025-03-18 21:06:45 -0500

    Update component properties

--- src/views/DatePicker.tsx
+++ src/views/DatePicker.tsx
@@ -1,10 +1,20 @@
 import '../styles/DatePicker.css'
 import { Calendar } from '@/components/ui/calendar'
+import { useState } from 'react'
 
 export function DatePicker () {
+  const [date, setDate] = useState<Date>()
+
   return (
     <article className='calendar-card'>
-      <Calendar />
+        <Calendar
+          mode="single"
+          captionLayout="dropdown"
+          selected={date}
+          onSelect={setDate}
+          fromYear={1960}
+          toYear={2030}
+        />
     </article>
   )
 }
