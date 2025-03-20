a104c440a2f72e4d7a51a37ac0f3350e24c8e2c8
Bryan Gordon <bryanbge92@gmail.com>
2025-03-19 21:34:04 -0500

    Update component

--- src/componets/Home.tsx
+++ src/componets/Home.tsx
@@ -1,5 +1,6 @@
 import { DataFinance } from '@/views/DataFinance'
 import { DatePicker } from '@/views/DatePicker'
+// import { useState } from 'react'
 
 export function Home () {
   return (
@@ -8,13 +9,24 @@ export function Home () {
         <h1 className='text-4xl text-center'>Finance App</h1>
       </header>
 
+      {/*
       <section>
         <DataFinance />
       </section>
-
+      */}
       <section>
         <DatePicker />
       </section>
+
+      {/*
+      <article className='my-0 mx-auto w-1/2 flex justify-center'>
+        <button
+          className='rounded-xl bg-stone-500/75 text-white cursor-pointer px-4 py-2 my-5 text-center w-1/2'
+        >
+          Registrar
+        </button>
+      </article>
+      */}
     </main>
   )
 }
