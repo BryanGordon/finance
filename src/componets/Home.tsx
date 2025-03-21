c00beb430bf6b63ca82d1aa70f8673d99b93d094
Bryan Gordon <bryanbge92@gmail.com>
2025-03-21 18:17:57 -0500

    Update component

--- src/componets/Home.tsx
+++ src/componets/Home.tsx
@@ -1,24 +1,31 @@
+import { useTests } from '@/hooks/useTests'
 import { CrossIcon } from '@/icons/cross'
 import { DataFinance } from '@/views/DataFinance'
 import { DatePicker } from '@/views/DatePicker'
+import { useEffect } from 'react'
 // import { useState } from 'react'
 
 export function Home () {
+  const { dataStoredInfo, setDataStored } = useTests()
+
+  useEffect(() => {
+    setDataStored([45,45])
+  }, [])
+
   return (
     <main>
       <header>
         <h1 className='text-4xl text-center'>Finance App</h1>
       </header>
 
-      {/*
       <section>
         <DataFinance />
       </section>
-      */}
+      {/*
       <section>
         <DatePicker />
       </section>
-
+        */}
       {/*
       <article className='my-0 mx-auto w-1/2 flex justify-center'>
         <button
@@ -31,6 +38,14 @@ export function Home () {
       <div className='bg-white'>
         <CrossIcon />
       </div>
+        {
+          dataStoredInfo.map((item, index) => (
+            <div key={index} className='bg-slate-600'>
+              <p className='text-lg text-white'>{item}</p>
+            </div>
+
+          ))
+        }
     </main>
   )
 }
