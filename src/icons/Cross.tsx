7c75e4ebf666e4d8613931d32e0dca109b4d8f1e
Bryan Gordon <bryanbge92@gmail.com>
2025-03-25 18:10:13 -0500

    Format icon code

--- src/icons/Cross.tsx
+++ src/icons/Cross.tsx
@@ -1,18 +1,18 @@
 export function CrossIcon () {
   return (
     <svg
-      width="27"  
-      height="27"  
-      viewBox="0 0 24 24"  
-      fill="none"  
-      stroke="#FB5448"  
-      stroke-width="1.75"  
-      stroke-linecap="round"  
-      stroke-linejoin="round"  
+      width='27'
+      height='27'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='#fb5448'
+      stroke-width='1.75'
+      stroke-linecap='round'
+      stroke-linejoin='round'
     >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-      <path d="M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z" />
-      <path d="M9 9l6 6m0 -6l-6 6" />
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z' />
+      <path d='M9 9l6 6m0 -6l-6 6' />
     </svg>
   )
 }
