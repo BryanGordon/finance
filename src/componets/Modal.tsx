4848fdd4ba7dc6d760911d50adc1474a7d33e1e1
Bryan Gordon <bryanbge92@gmail.com>
2025-03-25 18:26:48 -0500

    Add some styles

--- src/componets/Modal.tsx
+++ src/componets/Modal.tsx
@@ -1,8 +1,11 @@
+import { CrossIcon } from '@/icons/cross'
+
 export function Modal () {
   return (
-    <article>
-      <div>
+    <article className='relative bg-stone-500/50'>
+      <div className='p-20 bg-blue-200/25 rounded-lg'>
         <h1>Informacion modal</h1>
+        <CrossIcon />
       </div>
     </article>
   )
