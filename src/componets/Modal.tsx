35a643d4de6d34260e77da317e7b18856e981a92
Bryan Gordon <bryanbge92@gmail.com>
2025-03-26 21:20:38 -0500

    Create modal
    - Add styles
    - Create structure

--- src/componets/Modal.tsx
+++ src/componets/Modal.tsx
@@ -1,11 +1,27 @@
 import { CrossIcon } from '@/icons/cross'
+import { useState } from 'react'
 
 export function Modal () {
+  const [active, setActive] = useState(false)
+  console.log('value ' + active)
+
   return (
-    <article className='relative bg-stone-500/50'>
-      <div className='p-20 bg-blue-200/25 rounded-lg'>
+    <article className='fixed bg-stone-500/50 w-dvw h-dvh m-0 content-center'>
+      <div className='p-20 bg-blue-200/25 rounded-lg text-red-800 content-center w-1/2 h-1/4 mx-auto'>
+        <div className='flex justify-end mb-5'>
+          <button onClick={() => setActive(!active)}>
+            <CrossIcon />
+          </button>
+        </div>
         <h1>Informacion modal</h1>
-        <CrossIcon />
+        <div className='flex flex-wrap space-x-5 space-y-2.5'>
+          <strong>Fecha de pago:</strong>
+          <span>26/03/2025</span>
+          <strong>Motivo del pago:</strong>
+          <span>Lorem ipsum dolor sit amet.</span>
+          <strong>Detalles del deposito</strong>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi in laboriosam blanditiis natus iure velit!</p>
+        </div>
       </div>
     </article>
   )
